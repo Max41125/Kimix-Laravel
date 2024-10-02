4723dbc4623040f3cfc2c49757c509235dfd8a44
Max41125 <desyatkov.maxim@gmail.com>
2024-10-02 15:45:21 +0500

    CORS TOKEN

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
@@ -19,7 +19,7 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
 
