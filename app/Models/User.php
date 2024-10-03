c49f8377767e21421d008dac0c0d7f56486f575e
Max41125 <desyatkov.maxim@gmail.com>
2024-10-03 05:00:07 +0500

    test

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
 
-class User extends Model
+class User extends Authenticatable
 {
     use HasFactory, HasApiTokens, Notifiable;
 
@@ -16,6 +16,11 @@ class User extends Model
         'name',
         'email',
         'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
     ];
 
     /**
