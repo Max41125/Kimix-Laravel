3b25564731388037096bbadd07571495198f64c4
Max41125 <desyatkov.maxim@gmail.com>
2024-10-06 16:45:42 +0500

    test

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log; // Добавь эту строку
 
 class AuthenticatedSessionController extends Controller
 {
@@ -14,18 +15,14 @@ class AuthenticatedSessionController extends Controller
             'email' => 'required|email',
             'password' => 'required',
         ]);
-    
+
         if (Auth::attempt($credentials)) {
             return response()->json(['message' => 'Login successful'], 200);
-        } else {
-            Log::error('Login failed for email: ' . $credentials['email']);
-            return response()->json(['message' => 'Invalid credentials'], 401);
         }
-        
-    
+
+        Log::error('Login failed for email: ' . $credentials['email']); // Используй класс Log
         return response()->json(['message' => 'Invalid credentials'], 401);
     }
-    
 
     public function destroy(Request $request)
     {
