4993c29ae4ba8c1f5061bc3f11f15890968a40ba
Max41125 <desyatkov.maxim@gmail.com>
2024-10-07 16:32:35 +0500

    test registr

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,11 @@ Route::middleware(['auth:sanctum'])->group(function () {
         return $request->user();
     });
 
-    Route::get('/email/verify/{id}/{hash}', [VerificationController::class, '__invoke'])
-    ->middleware(['signed'])
-    ->name('verification.verify');
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+    
+        return response()->json(['message' => 'Email verified successfully.'], 200);
+    })->name('verification.verify');
 
 
     Route::get('/protected-route', [YourProtectedController::class, 'index']);
