a6a56399f812a34ef06be348abdcf900b5a69b56
Max41125 <desyatkov.maxim@gmail.com>
2024-10-07 15:39:32 +0500

    fix

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [AuthenticatedSessionController::class, 'create'])
-    ->name('login');
-
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-
-Route::get('/register', [RegisteredUserController::class, 'create'])
-    ->name('register');
-
-Route::post('/register', [RegisteredUserController::class, 'store']);
