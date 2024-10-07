957d92ccfc54779341627cf8f19d8c3894d555c5
Max41125 <desyatkov.maxim@gmail.com>
2024-10-07 16:43:08 +0500

    EmailVerificationRequest

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
-
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 Route::get('/', function () {
     return view('welcome');
 });
