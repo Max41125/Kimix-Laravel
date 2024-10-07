534d70c1b4cf8d8115b5bde63a83c199b30ccdb3
Max41125 <desyatkov.maxim@gmail.com>
2024-10-07 19:33:04 +0500

    web route

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
+
 Route::get('/email/verify', function () {
     return view('auth.verify-email');
 })->middleware('auth')->name('verification.notice');
