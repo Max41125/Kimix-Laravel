a3b1b10120914eeb8a4e17a9c95468da3986d900
Max41125 <desyatkov.maxim@gmail.com>
2024-10-11 12:30:22 +0500

    new generator CSRF

--- app/Http/Controllers/Auth/CustomCsrfCookieController.php
+++ app/Http/Controllers/Auth/CustomCsrfCookieController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
+use Illuminate\Support\Facades\Cookie;
 
 class CustomCsrfCookieController extends CsrfCookieController
 {
@@ -17,12 +18,18 @@ class CustomCsrfCookieController extends CsrfCookieController
      */
     public function show(Request $request)
     {
-        // Если запрос ожидает JSON, возвращаем ответ 200
+        // Генерируем CSRF токен
+        $csrfToken = csrf_token();
+
+        // Устанавливаем CSRF токен в куки
+        $cookie = Cookie::make('XSRF-TOKEN', $csrfToken, 120, null, null, true, true);
+
+        // Если запрос ожидает JSON, возвращаем ответ 200 с CSRF токеном
         if ($request->expectsJson()) {
-            return new JsonResponse(['message' => 'CSRF token set'], 200);
+            return (new JsonResponse(['csrfToken' => $csrfToken], 200))->withCookie($cookie);
         }
 
         // В противном случае возвращаем обычный ответ с кодом 200
-        return new Response(['message' => 'CSRF token set'], 200);
+        return (new Response(['message' => 'CSRF token set'], 200))->withCookie($cookie);
     }
 }
