d88fade4850a546b62bf8a6daea0cf46ee2f93df
Max41125 <desyatkov.maxim@gmail.com>
2024-10-11 10:52:37 +0500

    Middleware test

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class VerifyCsrfToken extends Middleware
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array<int, string>
+     */
+    protected $except = [
+        //
+    ];
+
+    //add our custom getTokenFromRequest function
+    protected function getTokenFromRequest($request)
+    {
+        //keep default behavior, use parent method first
+        $token = parent::getTokenFromRequest($request);
+        //if token not found we will get token from cookie
+        if (!$token) {
+            $token = $request->cookie('XSRF-TOKEN');
+        }
+        return $token;
+    }
+}
