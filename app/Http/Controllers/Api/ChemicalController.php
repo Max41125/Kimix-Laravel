dd30c5d415af4798add58db312fec2edab37b248
Max41125 <desyatkov.maxim@gmail.com>
2024-10-21 11:29:45 +0500

    add user id

--- app/Http/Controllers/Api/ChemicalController.php
+++ app/Http/Controllers/Api/ChemicalController.php
@@ -94,7 +94,7 @@ class ChemicalController extends Controller
         $chemical = Chemical::findOrFail($chemicalId);
         
         // Получаем поставщиков с данными из таблицы pivot (chemical_user)
-        $suppliers = $chemical->users()->select('users.name', 'chemical_user.unit_type', 'chemical_user.price', 'chemical_user.currency')
+        $suppliers = $chemical->users()->select('users.id','users.name', 'chemical_user.unit_type', 'chemical_user.price', 'chemical_user.currency')
             ->get();
         
         return response()->json($suppliers, 200);
