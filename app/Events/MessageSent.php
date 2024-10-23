291a084bf2040bd74cf32ee0c7d718ba52179047
Max41125 <desyatkov.maxim@gmail.com>
2024-10-23 17:15:25 +0500

    test

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -2,11 +2,15 @@
 
 namespace App\Events;
 
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+
 use Illuminate\Support\Facades\Log;
 class MessageSent implements ShouldBroadcast
 {
