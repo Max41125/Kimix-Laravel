b61951607d6d8a08c7f22c11a905c5261335aa2b
Max41125 <desyatkov.maxim@gmail.com>
2024-10-24 04:53:38 +0500

    test

--- routes/channels.php
+++ routes/channels.php
@@ -5,22 +5,11 @@ use Illuminate\Support\Facades\Broadcast;
 use App\Models\Order;
 use App\Models\User;
 use Illuminate\Support\Facades\Log;
-/*
-|--------------------------------------------------------------------------
-| Broadcast Channels
-|--------------------------------------------------------------------------
-|
-| Here you may register all of the event broadcasting channels that your
-| application supports. The given channel authorization callbacks are
-| used to check if an authenticated user can listen to the channel.
-|
-*/
 
- 
-Broadcast::channel('private-chat.{orderId}', function ($user, $orderId) {
-    \Log::info('Checking access for user ' . $user->id . ' to order ' . $orderId);
-    $orderOwnerId = getOrderOwnerId($orderId);
-    \Log::info('Order owner is: ' . $orderOwnerId);
-    
-    return (int) $user->id === (int) $orderOwnerId;
+Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('private-chat.{orderId}', function (User $user, int $orderId) {
+    return $user->id === Order::findOrNew($orderId)->user_id;
 });
