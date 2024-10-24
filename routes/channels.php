9c335a973b57b3c617ea93fe3331dcfefb86005f
Max41125 <desyatkov.maxim@gmail.com>
2024-10-24 05:07:59 +0500

    test

--- routes/channels.php
+++ routes/channels.php
@@ -1,15 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
-
 use App\Models\Order;
 use App\Models\User;
-use Illuminate\Support\Facades\Log;
 
+// Канал для пользователей (пример трансляции личных уведомлений)
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
+// Приватный канал чата для заказов
 Broadcast::channel('private-chat.{orderId}', function (User $user, int $orderId) {
+    // Найдем заказ и проверим, является ли пользователь владельцем заказа
     return $user->id === Order::findOrNew($orderId)->user_id;
 });
